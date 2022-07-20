f17d4e50ed3e3dbe86870f0693a967a6a358eb29
ImranHossain5722 <imd572258@gmail.com>
2022-07-20 21:56:37 +0600

    worknav

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="App">
-      <Navbar>
-          <Routes>
-            <Route path="/" element={<Home></Home>}> </Route>
-          </Routes>
-
-      </Navbar>
+      
 
 
 

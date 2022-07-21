b7b7a96624b8c43365846353e0757c1fbc65e996
Noor-26 <nk26982698@gmail.com>
2022-07-21 19:22:56 +0600

    review section done

--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,63 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
+
+// import required modules
+import { Navigation } from "swiper";
 import './review.css'
-import { EffectCoverflow, Pagination } from "swiper";
 
 const Review = () => {
+  const [reviews, setReviews] = useState([])
+  useEffect(() => {
+    fetch('reviews/review.json')
+      .then(res => res.json())
+      .then(data => setReviews(data))
+  }, [])
+
+
   return (
-    <div>
- <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
+    <div className="p-5 my-20">
+      <Swiper navigation={true} slidesPerView={3} loop={true} grabCursor={true}
         centeredSlides={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 
-          0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2,
-          slideShadows: true,
-        }}
-      loop={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
+        spaceBetween={30} modules={[Navigation]} className="mySwiper">
+        {
+          reviews.map(review => <SwiperSlide>
+            <p className="p-2">{review.review}</p>
+            <div class="avatar p-2">
+              <div class="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src={review.image} alt="" />
+              </div>
+              <p className="pl-4 pt-2">{review.name}</p>
+            </div>
+
+
+          </SwiperSlide>)
+        }
       </Swiper>
     </div>
   )

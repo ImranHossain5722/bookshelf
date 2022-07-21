6bc1bf736db1bac683230512886c40892d2daef2
ImranHossain5722 <imd572258@gmail.com>
2022-07-21 17:11:47 +0600

    build contactClubBanner section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import ContactClubBanner from "../../components/ContactClubBanner/ClubBanner"
 const Home = () => {
     return (
         <div>
             Home 
+            <ContactClubBanner></ContactClubBanner>
         </div>
     );
 };

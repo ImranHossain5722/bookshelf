3d2c15f50e639d28b25158064ca0374c5232d1db
spn.arafath <spn8@spondonit.com>
2022-07-21 21:50:58 +0600

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#057AFF",
+
+          secondary: "#463AA1",
+
+          accent: "#C149AD",
+
+          neutral: "#021431",
+
+          "base-100": "#FFFFFF",
+
+          info: "#93E6FB",
+
+          success: "#80CED1",
+
+          warning: "#EFD8BD",
+
+          error: "#E58B8B",
+          myColor: "#7510f7",
+        },
+      },
+      "dark",
+    ],
   },
   plugins: [require("daisyui")],
 };

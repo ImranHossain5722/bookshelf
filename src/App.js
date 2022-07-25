62234f2697487ec096290a583177b0fb21d0044d
spn.arafath <spn8@spondonit.com>
2022-07-25 22:11:04 +0600

    cart page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 // aos animation
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import NavBar from "./components/Navbar/NavBar";
 import SampleCard from "./components/SampleCard/SampleCard";
 import Home from "./pages/home/Home";
 import Footer from "./components/Footer/Footer";
+import Cart from "./pages/Cart/Cart";
 
 // initialize aos
 AOS.init();
@@ -16,19 +17,13 @@ function App() {
     <div className="App">
       <NavBar>
         <Routes>
-          <Route path="/" element={<Home></Home>}>
-
-          </Route>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/cart" element={<Cart></Cart>}></Route>
         </Routes>
-        
-        <SampleCard />
 
+        {/* <SampleCard /> */}
       </NavBar>
-
       <Footer></Footer>
-          
-   
-      
     </div>
   );
 }

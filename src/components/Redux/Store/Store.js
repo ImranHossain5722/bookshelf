dc031763bfe2e4e8716abd383bc23964b24f51a6
Noor-26 <nk26982698@gmail.com>
2022-07-29 09:30:31 +0600

    all books reducer

--- src/components/Redux/Store/Store.js
+++ src/components/Redux/Store/Store.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
-import { sellBookReducer,addCartReducer, authorsReducer,popularBookReducer, categoryReducer, allUserReducer, allPublisherReducer, allAuthorReducer, allOrdersReducer, myorderReducer, myProfileReducer, orderHistoryReducer } from '../Features/bookReducers';
+import { sellBookReducer,addCartReducer, authorsReducer,popularBookReducer, categoryReducer, allUserReducer, allPublisherReducer, allAuthorReducer, allOrdersReducer, myorderReducer, myProfileReducer, orderHistoryReducer, selectedBooksReducer, cartBooksReducer, allBooksReducer } from '../Features/bookReducers';
 const thunk = require('redux-thunk');
 
 const middleware = process.env.NODE_ENV !== 'production' ?
@@ -10,6 +10,7 @@ const middleware = process.env.NODE_ENV !== 'production' ?
 const store = configureStore({
     reducer: {
         sellBooks : sellBookReducer,
+        allBooks : allBooksReducer,
         addCart : addCartReducer,
         author: authorsReducer ,
         popularBooks : popularBookReducer,
@@ -20,7 +21,10 @@ const store = configureStore({
         allOrders : allOrdersReducer,
         myOrder : myorderReducer,
         myProfile : myProfileReducer,
-        OrderHistory : orderHistoryReducer
+        OrderHistory : orderHistoryReducer,
+        cartBooks : cartBooksReducer,
+        selectedBooks : selectedBooksReducer
+
     }
     
 },applyMiddleware(...middleware)

a9080953295dcdd81c3f1e00b52f375e65b23936
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-08-03 12:08:03 +0600

    button icon added to club banner page

--- src/components/AddBanner/AddBanner.js
+++ src/components/AddBanner/AddBanner.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Bannerbook from "../../Assets/images/Banner-images/img1-13.png";
+import Button from "../Button/Button";
 
 const AddBanner = () => {
   //dynamic api needed to make the banner image and link change form the dashboard
@@ -15,8 +16,8 @@ const AddBanner = () => {
         </span></h2>
         <p className="pl-28 text-4xl lg:text-7xl font-semibold text-primary">Sale -25%</p>
         <p className="pl-28 text-4xl font-semibold text-accent">ON ORDER OVER</p>
-        <div class="card-actions">
-          <button class="btn btn-primary text-white hover:text-white hover:bg-accent ml-28">SEE MORE </button>
+        <div class="card-actions pl-28">
+          <Button>SEE MORE</Button>
         </div>
       </div>
       <figure>

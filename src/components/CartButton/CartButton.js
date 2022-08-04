3aa26dfe87026a66c1b1487b6582c12d01bf3a9e
Abu Yusuf <imabuyusuf@gmail.com>
2022-08-04 20:05:17 +0600

    updates

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -6,26 +6,26 @@ import auth from '../../firebase.init';
 import { useSelector } from 'react-redux';
 
 
-const CartButton = ({_id}) => {
-    
-    const user = useSelector((state) => state?.newUser?.user)
-    const userId = user?._id
-    const AddCart =(id) => {
-      const cartData = {
-        user_id : userId,
-        cart_data : {
-            book : id,
-            qnt : 2
-        }
+const CartButton = ({ _id }) => {
+
+  const user = useSelector((state) => state?.newUser?.user)
+  const userId = user?._id
+  const AddCart = (id) => {
+    const cartData = {
+      user_id: userId,
+      cart_data: {
+        book: id,
+        qnt: 2
+      }
     }
-    axios.post('https://book-shelf-webapp.herokuapp.com/add-to-cart',cartData).then(data => console.log(data))
+    axios.post('https://book-shelf-webapp.herokuapp.com/add-to-cart', cartData).then(data => console.log(data))
     // console.log(cartData)
-      }  
-      
-    
+  }
+
+
   return (
     <button className="text-3xl text-white hover:text-primary duration-500">
-    <FaShoppingCart onClick={() => AddCart(_id)}/>
+      <FaShoppingCart onClick={() => AddCart(_id)} />
     </button>
   )
 }
--- src/components/Category/Categorys.js
+++ src/components/Category/Categorys.js
@@ -1,10 +1,7 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// images
-import img1 from '../../Assets/images/category-images/Open Book.png';
-import img2 from '../../Assets/images/category-images/Vector.png';
-import img3 from '../../Assets/images/category-images/Computer.png';
-import img4 from '../../Assets/images/category-images/Done.png';
+//icons
+import { FaHeart, FaEye } from 'react-icons/fa';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -17,63 +14,45 @@ import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper";
 import useWindowDimensions from "../windowSize/windowSize";
+import { useSelector } from "react-redux";
+import Stars from "../Stars/Stars";
+import CartButton from "../CartButton/CartButton";
+import { NavLink } from "react-router-dom";
 
 
-// fake data
-const categories = [
-  {
-    _id: 1,
-    image: img1,
-    name: "CHILDREN'S BOOK",
-  },
-  {
-    _id: 2,
-    image: img2,
-    name: "SCIENCE FICTION",
-  },
-  {
-    _id: 3,
-    image: img3,
-    name: "COMPUTER BOOKS",
-  },
-  {
-    _id: 4,
-    image: img1,
-    name: "CHILDREN'S BOOK",
-  },
-  {
-    _id: 5,
-    image: img1,
-    name: "CHILDREN'S BOOK",
-  },
-  {
-    _id: 6,
-    image: img2,
-    name: "SCIENCE FICTION",
-  },
-  {
-    _id: 7,
-    image: img3,
-    name: "COMPUTER BOOKS",
-  },
-  {
-    _id: 8,
-    image: img1,
-    name: "CHILDREN'S BOOK",
-  },
-  {
-    _id: 9,
-    image: img2,
-    name: "SCIENCE FICTION",
-  },
-  {
-    _id: 10,
-    image: img3,
-    name: "COMPUTER BOOKS",
-  },
-];
+
 
 const Categorys = () => {
+  const books = useSelector((state) => state?.sellBooks?.books);
+  // Get Categories from database 
+  const [allCategories, setAllCategories] = useState([]);
+
+  useEffect(() => {
+    const options = { method: 'GET' };
+    fetch('https://book-shelf-webapp.herokuapp.com/all-categories', options)
+      .then(response => response.json())
+      .then(data => setAllCategories(data))
+    // .catch(err => console.error(err));
+
+  }, [])
+
+  console.log(allCategories);
+
+  const [selectedCatId, setSelectedCatId] = useState('');
+
+
+  console.log(selectedCatId);
+  // get selected Category 
+  const getCategoryIdOnClick = (id) => {
+    if (selectedCatId.length <= 0) {
+      setSelectedCatId(id)
+    } else {
+      setSelectedCatId('')
+    }
+
+  }
+
+
   const [size, setSize] = useState(1);
   const { width } = useWindowDimensions();
 
@@ -92,6 +71,7 @@ const Categorys = () => {
       setSize(1)
     }
   }, [width]);
+
   return (
     <div className="mt-[60px] lg:mt-[120px]">
       {/* ------title section----- */}
@@ -114,17 +94,63 @@ const Categorys = () => {
           style={{ "--swiper-theme-color": "#27AE61" }}
         >
           {
-            categories.map(category => <SwiperSlide key={category._id}>
-              <div className="h-56 w-52 bg-[#27AE612B] rounded-lg flex items-center justify-center p-2">
+            allCategories.map(category => <SwiperSlide key={category._id}>
+              <div onClick={() => getCategoryIdOnClick(category._id)} className="h-56 w-52 bg-[#27AE612B] rounded-lg flex items-center justify-center p-2">
                 <div>
-                  <img src={category.image} className="mx-auto" alt="" />
-                  <h2 className="text-2xl text-center text-[#00124E] font-bold mt-2">{category.name}</h2>
+                  <img src={category?.category_icon_url} className="mx-auto" width={70} alt="" />
+                  <h2 className="text-2xl mt-6 text-center text-[#00124E] font-bold">{category?.category_title}</h2>
                 </div>
               </div>
             </SwiperSlide>)
           }
         </Swiper>
       </div>
+      {/* show book after category select  */}
+
+      {selectedCatId &&
+        <div className="bg-white max-w-[1240px] mx-auto mt-[10px] lg:mt-[10px] py-10">
+          <div className="mt-8">
+            <Swiper
+              slidesPerView={size}
+              spaceBetween={30}
+              slidesPerGroup={size}
+              loop={true}
+              loopFillGroupWithBlank={true}
+              navigation={true}
+              modules={[Navigation]}
+              className="mySwiper px-7 py-6"
+              style={{ "--swiper-theme-color": "#27AE61" }}
+            >
+              {
+                books?.map(book => <SwiperSlide key={book._id}>
+                  <NavLink to={`/selectedBook/${book._id}`}>
+                    <div className="book-shadow rounded-lg h-[460px] pt-6 flex justify-center">
+                      <div className="for-hover relative">
+                        {/* relative */}
+                        <img src={book.book_cover_photo_url} className="h-64 w-44 image-full" alt="" />
+                        {/* absolute hover effect */}
+                        <div className="bg-[#00124ea4] h-64 w-44 flex items-center justify-center absolute top-0 hover-button hidden">
+                          <button className="text-3xl text-white hover:text-primary duration-500">
+                            <FaEye />
+                          </button>
+                          <button className="mx-5 text-3xl text-white hover:text-primary duration-500">
+                            <FaHeart />
+                          </button>
+                          <CartButton _id={book._id} />
+                        </div>
+                        <div className="w-44 mt-2">
+                          <h3>{book.book_title}</h3>
+                          <h2 className="text-xl font-semibold text-primary mt-2 mb-1">${book.book_price}</h2>
+                          <Stars />
+                        </div>
+                      </div>
+                    </div>
+                  </NavLink>
+                </SwiperSlide>)
+              }
+            </Swiper>
+          </div>
+        </div>}
     </div>
   );
 };

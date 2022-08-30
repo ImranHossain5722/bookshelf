1f3bf69e091a2a060b2731eb65b9104df5bc54af
spn.arafath <spn8@spondonit.com>
2022-08-31 00:54:24 +0600

    add new pages

--- src/components/Books/AllBooks.js
+++ src/components/Books/AllBooks.js
@@ -7,28 +7,27 @@ import { FaPlus, FaMinus } from "react-icons/fa";
 import Wishlistbutton from "../wishlistButton/Wishlistbutton";
 import QuickViewButton from "../QuickViewButton/QuickViewButton";
 import AddCartButton from "../AddCartButton/AddCartButton";
-import { FaHome } from 'react-icons/fa';
-import { GiBookCover } from 'react-icons/gi'; 
-import { RiLayoutGridFill } from 'react-icons/ri'; 
-import { AiOutlineUnorderedList } from 'react-icons/ai'; 
+import { FaHome } from "react-icons/fa";
+import { GiBookCover } from "react-icons/gi";
+import { RiLayoutGridFill } from "react-icons/ri";
+import { AiOutlineUnorderedList } from "react-icons/ai";
 import { FaCartPlus, FaHeart } from "react-icons/fa";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
 const AllBooks = () => {
-  
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [authors, setAuthors] = useState([]);     
+  const [authors, setAuthors] = useState([]);
   const [hidden, setHidden] = useState(false);
   const [active, setActive] = useState(false);
   const [countBooks, setCountBooks] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setpostPerPage] = useState(10);
   const [showList, setShowLIst] = useState(false);
-  const user = useSelector((state) => state?.newUser?.user)
-  const userId = user?._id
+  const user = useSelector((state) => state?.newUser?.user);
+  const userId = user?._id;
   useEffect(() => {
     const loadBooks = async () => {
       setLoading(true);
@@ -62,8 +61,6 @@ const AllBooks = () => {
     };
 
     loadAuthors();
-
-   
   }, []);
 
   // filtering all books by category or author
@@ -125,64 +122,74 @@ const AllBooks = () => {
       user_id: userId,
       cart_data: {
         book: id,
-        qnt: 1
-      }
-    }
+        qnt: 1,
+      },
+    };
     if (userId) {
-
-      axios.post('https://book-shelf-webapp.herokuapp.com/add-to-cart', cartData).then(data => { toast.success('successfully added to cart') })
+      axios
+        .post("https://book-shelf-webapp.herokuapp.com/add-to-cart", cartData)
+        .then((data) => {
+          toast.success("successfully added to cart");
+        });
     } else {
-      console.log("user id not found", userId)
+      console.log("user id not found", userId);
     }
-  }
+  };
 
-  // adding to  wishlist 
+  // adding to  wishlist
   const AddWishlist = async (id) => {
     const cardData = {
       user_id: userId,
       wishlist_data: {
-        book: id
-      }
-    }
+        book: id,
+      },
+    };
     if (userId) {
-      await axios.post('https://book-shelf-webapp.herokuapp.com/add-to-wishlist', cardData).then(data => toast.success("added to wishlist"))
+      await axios
+        .post(
+          "https://book-shelf-webapp.herokuapp.com/add-to-wishlist",
+          cardData
+        )
+        .then((data) => toast.success("added to wishlist"));
     } else {
-      console.log(" the user id is not found")
+      console.log(" the user id is not found");
     }
-  }
-  
+  };
+
   return (
     <div className="section_padding">
-       <div className="flex w-full mb-4 justify-between  ">
-      <div class="text-sm  breadcrumbs justify-center ">
-  <ul className=" text-center mx-auto">
-    <li>
-      <Link to="/">
-      <FaHome className="text-[20px] mr-2"/>
-      
-      <p className="text-[20px] ">Home</p>
+      <div className="flex w-full mb-4 justify-between  ">
+        <div class="text-sm  breadcrumbs justify-center ">
+          <ul className=" text-center mx-auto">
+            <li>
+              <Link to="/">
+                <FaHome className="text-[20px] mr-2" />
 
-      </Link>
-    </li> 
-    <li>
-
-      <GiBookCover  className="text-[20px] mr-2"/>
-      <p className="text-[20px] ">Book</p>
-    </li>
-  </ul>
-</div>
-<div >
-<button className="btn btn-sm mr-2 text-primary border-primary border-2 hover:bg-primary hover:text-white hover:border-primary focus:bg-primary focus:text-white focus:border-primary" onClick={() => setShowLIst(false)}>
-  <RiLayoutGridFill className = "text-[18px] font-bold"/>
-</button>
-<button  className="btn btn-sm mr-2 text-primary border-primary border-2 hover:bg-primary hover:text-white hover:border-primary focus:bg-primary focus:text-white focus:border-primary" onClick={() => setShowLIst(true)}>
- <AiOutlineUnorderedList  className = "text-[18px] font-bold"/>
- </button>
-  
-  </div>
-</div>
+                <p className="text-[20px] ">Home</p>
+              </Link>
+            </li>
+            <li>
+              <GiBookCover className="text-[20px] mr-2" />
+              <p className="text-[20px] ">Book</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button
+            className="btn btn-sm mr-2 text-primary border-primary border-2 hover:bg-primary hover:text-white hover:border-primary focus:bg-primary focus:text-white focus:border-primary"
+            onClick={() => setShowLIst(false)}
+          >
+            <RiLayoutGridFill className="text-[18px] font-bold" />
+          </button>
+          <button
+            className="btn btn-sm mr-2 text-primary border-primary border-2 hover:bg-primary hover:text-white hover:border-primary focus:bg-primary focus:text-white focus:border-primary"
+            onClick={() => setShowLIst(true)}
+          >
+            <AiOutlineUnorderedList className="text-[18px] font-bold" />
+          </button>
+        </div>
+      </div>
       <div style={{ background: "#FBF6F6" }} className=" container mx-auto ">
-
         <div className="md:flex gap-6 items-start ">
           {/* filter options left-side */}
           <div className="flex-1 md:w-1/4 mb-4">
@@ -272,107 +279,123 @@ const AllBooks = () => {
             </div>
           </div>
           {/* filter results right-side */}
-        {
-          showList ?  <div className="grid grid-cols-1 lg:grid-cols-1 gap-6 mb-10 md:w-3/4">
-          {loading ? (
-            <Loading />
+          {showList ? (
+            <div className="grid grid-cols-1 lg:grid-cols-1 gap-6 mb-10 md:w-3/4">
+              {loading ? (
+                <Loading />
+              ) : (
+                currentPosts?.map((book) => (
+                  <div class="product_widget26  bg-white lg:flex gap-2">
+                    <div className="product_thumb_upper position-relative">
+                      {book.discount > 0 && (
+                        <span className="offer_badge">-{book.discount}%</span>
+                      )}
+                      <Link
+                        to={`/selectedBook/${book?._id}`}
+                        className="thumb text-center"
+                      >
+                        <img
+                          src={book?.book_cover_photo_url}
+                          alt=""
+                          className="min-w-[300px]"
+                        />
+                      </Link>
+                      <div className="product_action">
+                        <Wishlistbutton _id={book._id} />
+                        <QuickViewButton _id={book._id} />
+                        <CartButton _id={book._id} />
+                      </div>
+                    </div>
+                    <div class="product__meta my-10">
+                      <h2 class="card-title">
+                        <Link to={`/selectedBook/${book?._id}`}>
+                          <h4>{book.book_title}</h4>
+                        </Link>
+                      </h2>
+                      <p className="h-fit mb-1">{book.book_description}</p>
+                      <div className="product_prise">
+                        <p>${book.book_price}</p>
+                      </div>
+                      <div className="stars">
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <span className="text-sm font-medium">(02 Rating)</span>
+                      </div>
+                      <div className="flex items-center text-black mb-3 gap-2">
+                        <button
+                          className="icon-btn add-btn"
+                          onClick={() => AddCart(book._id)}
+                        >
+                          <FaCartPlus className="add-icon  text-primary text-2xl" />
+
+                          <div className="btn-txt">Add to cart</div>
+                        </button>
+
+                        <button
+                          className="icon-btn add-btn"
+                          onClick={() => AddWishlist(book._id)}
+                        >
+                          <FaHeart className="add-icon text-primary text-2xl" />
+                          <div className="btn-txt">Add to wishlist</div>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
           ) : (
-            currentPosts?.map((book) => (
-              <div class="card lg:card-side bg-base-100 shadow-xl">
-             <div className="product_thumb_upper position-relative">
-                   {book.discount > 0 && <span className="offer_badge">-{book.discount}%</span>}
-                  <Link to={`/selectedBook/${book?._id}`} className="thumb text-center">
-                    <img src={book?.book_cover_photo_url} alt=""className="min-w-[300px]" />
-                  </Link>
-                  <div className="product_action">
-                     <Wishlistbutton _id={book._id} />
-                    <QuickViewButton _id={book._id} />
-                    <CartButton _id={book._id} />
-                 </div>
-               </div>
-              <div class="card-body">
-                <h2 class="card-title"> 
-                  <Link to={`/selectedBook/${book?._id}`}>
-                <h4>{book.book_title}</h4>
-              </Link>
-              </h2>
-                <p className="h-fit mb-1">{book.book_description}</p>
-                <div className="product_prise">
-                <p>${book.book_price}</p>
-              </div>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <span className="text-sm font-medium">(02 Rating)</span>
-              </div>
-              <div className="flex items-center text-black mb-3 gap-2">
-          
-              <button className="icon-btn add-btn" onClick={() => AddCart(book._id)}>
-              <FaCartPlus className="add-icon  text-primary text-2xl" />
-
-                <div className="btn-txt">Add to cart</div>
-              </button>
-            
-
-              <button className="icon-btn add-btn" onClick={() => AddWishlist(book._id)}>
-                <FaHeart className="add-icon text-primary text-2xl" />
-                <div className="btn-txt">Add to wishlist</div>
-              </button>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mb-10 md:w-3/4">
+              {loading ? (
+                <Loading />
+              ) : (
+                currentPosts?.map((book) => (
+                  <div className="product_widget26 mb_30 bg-white">
+                    <div className="product_thumb_upper position-relative">
+                      {book.discount > 0 && (
+                        <span className="offer_badge">-{book.discount}%</span>
+                      )}
+                      <Link
+                        to={`/selectedBook/${book?._id}`}
+                        className="thumb text-center"
+                      >
+                        <img src={book?.book_cover_photo_url} alt="" />
+                      </Link>
+                      <div className="product_action">
+                        <Wishlistbutton _id={book._id} />
+                        <QuickViewButton _id={book._id} />
+                        <CartButton _id={book._id} />
+                      </div>
+                    </div>
+                    <div className="product__meta">
+                      <Link to={`/selectedBook/${book?._id}`}>
+                        <h4>{book.book_title}</h4>
+                      </Link>
+                      <p className="text-[16px] text-[#00124e] font-semibold">
+                        {book.author}
+                      </p>
+                      <div className="stars">
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <i className="fas fa-star"></i>
+                        <span className="text-sm font-medium">(02 Rating)</span>
+                      </div>
+                      <div className="product_prise">
+                        <p>${book.book_price}</p>
+                      </div>
+                      <AddCartButton _id={book._id} />
+                    </div>
+                  </div>
+                  // </Link>
+                ))
+              )}
             </div>
-              </div>
-            </div>
-           
-            ))
           )}
-        </div>
-      :  <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mb-10 md:w-3/4">
-      {loading ? (
-        <Loading />
-      ) : (
-        currentPosts?.map((book) => (
-        
-          <div className="product_widget26 mb_30 bg-white">
-            <div className="product_thumb_upper position-relative">
-                {book.discount>0 && <span className="offer_badge">-{book.discount}%</span>}
-              <Link to={`/selectedBook/${book?._id}`} className="thumb text-center">
-                <img src={book?.book_cover_photo_url} alt="" />
-              </Link>
-              <div className="product_action">
-                <Wishlistbutton _id={book._id} />
-                <QuickViewButton _id={book._id} />
-                <CartButton _id={book._id} />
-              </div>
-            </div>
-            <div className="product__meta">
-              <Link to={`/selectedBook/${book?._id}`}>
-                <h4>{book.book_title}</h4>
-              </Link>
-              <p className="text-[16px] text-[#00124e] font-semibold">
-                {book.author}
-              </p>
-              <div className="stars">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <span className="text-sm font-medium">(02 Rating)</span>
-              </div>
-              <div className="product_prise">
-                <p>${book.book_price}</p>
-              </div>
-              <AddCartButton _id={book._id} />
-            </div>
-          </div>
-          // </Link>
-        ))
-      )}
-    </div>
- 
-        } 
         </div>
 
         {/* pagenation */}
--- src/pages/Products_details/Products_details.js
+++ src/pages/Products_details/Products_details.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import './Product_details.css'
+import { useSelector } from "react-redux";
+import "./Product_details.css";
 import { FaCartPlus, FaHeart } from "react-icons/fa";
 import { toast } from "react-toastify";
+import RecentlyViewed from "../../components/RecentlyViewed/RecentlyViewed";
 const Products_details = () => {
   const { _id } = useParams();
   const [book, setbook] = useState({});
   const [quantity, setqnt] = useState(1);
 
-  
   const {
     book_cover_photo_url,
     book_title,
@@ -20,16 +20,15 @@ const Products_details = () => {
     book_pages,
     book_language,
     discount,
-    
   } = book;
-  console.log(book)
+  console.log(book);
   useEffect(() => {
     axios
       .get(`https://book-shelf-webapp.herokuapp.com/get-book?id=${_id}`)
       .then((data) => setbook(data.data[0]));
   }, []);
-  const user = useSelector((state) => state?.newUser?.user)
-  const userId = user?._id
+  const user = useSelector((state) => state?.newUser?.user);
+  const userId = user?._id;
   // adding to cart
 
   const AddCart = (id) => {
@@ -37,128 +36,144 @@ const Products_details = () => {
       user_id: userId,
       cart_data: {
         book: id,
-        qnt: quantity
-      }
-    }
+        qnt: quantity,
+      },
+    };
     if (userId) {
-
-      axios.post('https://book-shelf-webapp.herokuapp.com/add-to-cart', cartData).then(data => { toast.success('successfully added to cart') })
+      axios
+        .post("https://book-shelf-webapp.herokuapp.com/add-to-cart", cartData)
+        .then((data) => {
+          toast.success("successfully added to cart");
+        });
     } else {
-      console.log("user id not found", userId)
+      console.log("user id not found", userId);
     }
-  }
+  };
 
-  // adding to  wishlist 
+  // adding to  wishlist
   const AddWishlist = async (id) => {
     const cardData = {
       user_id: userId,
       wishlist_data: {
-        book: id
-      }
-    }
+        book: id,
+      },
+    };
     if (userId) {
-      await axios.post('https://book-shelf-webapp.herokuapp.com/add-to-wishlist', cardData).then(data => toast.success("added to wishlist"))
+      await axios
+        .post(
+          "https://book-shelf-webapp.herokuapp.com/add-to-wishlist",
+          cardData
+        )
+        .then((data) => toast.success("added to wishlist"));
     } else {
-      console.log(" the user id is not found")
+      console.log(" the user id is not found");
     }
-  }
-  
-  const increaseQnt =() => {
-  
-    setqnt(quantity+1)
- 
-  }
-  const decreaseQnt = () => {
-    if(quantity !==1){
-      setqnt(quantity-1)
+  };
 
+  const increaseQnt = () => {
+    setqnt(quantity + 1);
+  };
+  const decreaseQnt = () => {
+    if (quantity !== 1) {
+      setqnt(quantity - 1);
     }
-  }
-  console.log(quantity)
+  };
+  console.log(quantity);
   return (
-    <div className="pt-[60px] md:pt-[80px] lg:pt-[120px]  pb-[60px] md:pb-[80px] lg:pb-[120px] ">
-      <div className="container m-auto flex gap-6 justify-center">
-        <div className="w-5/12">
-          <div className="thumb w-full">
-            <img className="w-full" src={book_cover_photo_url} alt="" />
+    <>
+      <div className="pt-[60px] md:pt-[80px] lg:pt-[120px]  pb-[60px] md:pb-[80px] lg:pb-[120px] ">
+        <div className="container m-auto flex gap-6 justify-center">
+          <div className="w-5/12">
+            <div className="thumb w-full">
+              <img className="w-full" src={book_cover_photo_url} alt="" />
+            </div>
           </div>
-        </div>
-        <div className="w-6/12">
-          <div className="cart_summery ">
-            {/* <div className="flex items-center gap-2 mb-3">
+          <div className="w-6/12">
+            <div className="cart_summery ">
+              {/* <div className="flex items-center gap-2 mb-3">
               <p className="text-[16px]">Availability : 1 in stock</p>
             </div> */}
-            <h4 className="text-black text-[24px] capitalize font-semibold mb-3">
-              {book_title}
-            </h4>
-            <div className="flex items-center gap-2 mb-3">
-              <h3 className="text-black text-[18px]">${book_price}</h3>
-              <p className="text-[16px]">Tax included</p>
-            </div>
+              <h4 className="text-black text-[24px] capitalize font-semibold mb-3">
+                {book_title}
+              </h4>
+              <div className="flex items-center gap-2 mb-3">
+                <h3 className="text-black text-[18px]">${book_price}</h3>
+                <p className="text-[16px]">Tax included</p>
+              </div>
 
-            <div className="flex items-center text-black mb-3">
-              <p>(8k+ reviews)</p>
-            </div>
-            <div className="flex items-center gap-2 mb-3">
-              <p className="text-black text-[16px]">Quantity</p>
-              <div className="flex">
-              <button className="bg-[#f9f9fd] w-[40px] h-[40px] flex items-center justify-center rounded-none border-[#e1e2e6] border-solid border text-black" onClick={increaseQnt}>
-                        +
-                      </button>
-                      <input
-                        type="text"
-                        value={quantity}
+              <div className="flex items-center text-black mb-3">
+                <p>(8k+ reviews)</p>
+              </div>
+              <div className="flex items-center gap-2 mb-3">
+                <p className="text-black text-[16px]">Quantity</p>
+                <div className="flex">
+                  <button
+                    className="bg-[#f9f9fd] w-[40px] h-[40px] flex items-center justify-center rounded-none border-[#e1e2e6] border-solid border text-black"
+                    onClick={increaseQnt}
+                  >
+                    +
+                  </button>
+                  <input
+                    type="text"
+                    value={quantity}
+                    className="input  w-[50px] h-[40px] max-w-xs rounded-none text-center border-[#e1e2e6] border-solid border-y-1 border-x-0 text-black"
+                  />
+                  <button
+                    className="bg-[#f9f9fd] w-[40px] h-[40px] flex items-center justify-center rounded-none border-[#e1e2e6 border-solid border text-black"
+                    onClick={decreaseQnt}
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+              <div className="flex items-center text-black mb-3 gap-2">
+                <button
+                  className="icon-btn add-btn"
+                  onClick={() => AddCart(_id)}
+                >
+                  <FaCartPlus className="add-icon  text-primary text-2xl" />
+                  <div className="btn-txt">Add to cart</div>
+                </button>
 
-                        className="input  w-[50px] h-[40px] max-w-xs rounded-none text-center border-[#e1e2e6] border-solid border-y-1 border-x-0 text-black"
-                      /> 
-                      <button className="bg-[#f9f9fd] w-[40px] h-[40px] flex items-center justify-center rounded-none border-[#e1e2e6 border-solid border text-black" onClick={decreaseQnt} >
-                        -
-                      </button>
+                <button
+                  className="icon-btn add-btn"
+                  onClick={() => AddWishlist(_id)}
+                >
+                  <FaHeart className="add-icon text-primary text-2xl" />
+                  <div className="btn-txt">Add to wishlist</div>
+                </button>
               </div>
-            </div>
-            <div className="flex items-center text-black mb-3 gap-2">
-              
-              <button className="icon-btn add-btn" onClick={() => AddCart(_id)}>
-                <FaCartPlus className="add-icon  text-primary text-2xl" />
-                <div className="btn-txt">Add to cart</div>
-              </button>
-
-              <button className="icon-btn add-btn" onClick={() => AddWishlist(_id)}>
-                <FaHeart className="add-icon text-primary text-2xl" />
-                <div className="btn-txt">Add to wishlist</div>
-              </button>
-            </div>
-            <h5 className="text-black text-[30px] font-medium mt-8">
-              Guaranteed Safe Checkout
-            </h5>
-            <div className=" single_description_wrap border-b-[1px] border-[#e1e2e6] pb-4 mb-4">
-              <div className="description_box">
-                <p className="Information_text text-black mb-3">
-                  <span> Language: </span> {book_language}
-                </p>
-                <p className="Information_text text-black mb-3">
-                  {" "}
-                  <span>Pages: </span> {book_pages}
-                </p>
-                <p className="Information_text text-black mb-3">
-                  {" "}
-                  <span>Country: </span> {book_country}
-                </p>
-                <p className="Information_text text-black mb-3">
-                  {" "}
-                  <span>Tags: </span> Vintage, Awesome, Summer, Beachwear
-                </p>
+              <h5 className="text-black text-[30px] font-medium mt-8">
+                Guaranteed Safe Checkout
+              </h5>
+              <div className=" single_description_wrap border-b-[1px] border-[#e1e2e6] pb-4 mb-4">
+                <div className="description_box">
+                  <p className="Information_text text-black mb-3">
+                    <span> Language: </span> {book_language}
+                  </p>
+                  <p className="Information_text text-black mb-3">
+                    {" "}
+                    <span>Pages: </span> {book_pages}
+                  </p>
+                  <p className="Information_text text-black mb-3">
+                    {" "}
+                    <span>Country: </span> {book_country}
+                  </p>
+                  <p className="Information_text text-black mb-3">
+                    {" "}
+                    <span>Tags: </span> Vintage, Awesome, Summer, Beachwear
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="single_description_wrap border-b-[1px] border-[#e1e2e6] pb-4 mb-4">
-              <div className="details_title">
-                <h4 className="text-black text-[18px]">Description</h4>
+              <div className="single_description_wrap border-b-[1px] border-[#e1e2e6] pb-4 mb-4">
+                <div className="details_title">
+                  <h4 className="text-black text-[18px]">Description</h4>
+                </div>
+                <div className="description_box">
+                  <p className="mb-4">{book_description}</p>
+                </div>
               </div>
-              <div className="description_box">
-                <p className="mb-4">{book_description}</p>
-              </div>
-            </div>
-            {/* <div className="single_description_wrap border-b-[1px] border-[#e1e2e6] pb-4 mb-4">
+              {/* <div className="single_description_wrap border-b-[1px] border-[#e1e2e6] pb-4 mb-4">
               <div className="details_title">
                 <h4 className="text-black text-[18px]">
                   Additional Information
@@ -179,10 +194,12 @@ const Products_details = () => {
                 </p>
               </div>
             </div> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <RecentlyViewed />
+    </>
   );
 };
 

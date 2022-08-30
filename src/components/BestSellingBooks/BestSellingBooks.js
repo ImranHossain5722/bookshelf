b5af4408b567f464b112f1359c163276b600167d
Noor-26 <nk26982698@gmail.com>
2022-08-30 11:41:57 +0600

    toggle routes done

--- src/components/BestSellingBooks/BestSellingBooks.js
+++ src/components/BestSellingBooks/BestSellingBooks.js
@@ -48,7 +48,7 @@ const BestSellingBooks = () => {
     return <Loading/>;
   }
   return (<div>
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mb-10 md:w-3/4">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-10 ">
             {loading ? (
               <Loading />
             ) : (

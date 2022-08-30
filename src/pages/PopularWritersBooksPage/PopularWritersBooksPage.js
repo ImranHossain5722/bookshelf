d3860ee7cc60045c48b8018232df06648de9ae4b
ImranHossain5722 <imd572258@gmail.com>
2022-08-30 11:38:32 +0600

    populer page

--- src/pages/PopularWritersBooksPage/PopularWritersBooksPage.js
+++ src/pages/PopularWritersBooksPage/PopularWritersBooksPage.js
@@ -1,11 +1,75 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+//icons
+import { FaHeart, FaEye, FaShoppingCart, FaRegEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import AddCartButton from "../../components/AddCartButton/AddCartButton";
+import Button from "../../components/Button/Button";
+import CartButton from "../../components/CartButton/CartButton";
+import Loading from "../../components/Loading/Loading";
+import QuickViewButton from "../../components//QuickViewButton/QuickViewButton";
+import Stars from "../../components/Stars/Stars";
+import Wishlistbutton from "../../components/wishlistButton/Wishlistbutton";
 const PopularWritersBooksPage = () => {
-    return (
-        <div>
-            Poplerwriters
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://book-shelf-webapp.herokuapp.com/all-books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+    setLoading(false);
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {books?.splice(0, 8).map((book) => (
+            <div className="product_widget26 mb_30 bg-white">
+              <div className="product_thumb_upper position-relative">
+                {book.discount > 0 && (
+                  <span className="offer_badge">-{book.discount}%</span>
+                )}
+                <Link
+                  to={`/selectedBook/${book?._id}`}
+                  className="thumb text-center"
+                >
+                  <img src={book.book_cover_photo_url} alt="" />
+                </Link>
+                <div className="product_action">
+                  <Wishlistbutton _id={book._id} />
+                  <QuickViewButton _id={book._id} />
+                  <CartButton _id={book._id} />
+                </div>
+              </div>
+              <div className="product__meta">
+                <Link to={`/selectedBook/${book?._id}`}>
+                  <h4>{book.book_title}</h4>
+                </Link>
+                <p className="text-[16px] text-[#00124e] font-semibold">
+                  {book?.book_author?.author_name}
+                </p>
+                <div className="stars">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <span className="text-sm font-medium">(02 Rating)</span>
+                </div>
+                <div className="product_prise">
+                  <p>${book.book_price}</p>
+                </div>
+                <AddCartButton _id={book._id} />
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      </>
+    </div>
+  );
 };
 
 export default PopularWritersBooksPage;
